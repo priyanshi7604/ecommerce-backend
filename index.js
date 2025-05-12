4a0b8749d28cb6555df9cf771a25140f3ce0730f
priyanshi7604 <priyanshi7604@gmail.com>
2025-05-12 13:48:36 +0530

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+const serverless = require('serverless-http')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send("success");
+})
+
+
+module.exports.handler = serverless(app)
